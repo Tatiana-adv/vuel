89b48bd844b428a19a08d389f384f87533b2094a
Tatiana-adv <arlualimi@gmail.com>
2025-10-30 08:58:58 -0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css' // Importa o CSS principal
+import App from './App.vue'
+
+// Cria a instância da aplicação Vue e monta no elemento com id="app"
+createApp(App).mount('#app')
